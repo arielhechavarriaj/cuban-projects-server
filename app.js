2dea0e2e57e58fb1ecc058ecae3ace8ba0c5a77f
leiraStudio <leiraStudio@gmail.com>
2022-02-18 00:40:35 -0500

    update app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
 const path = require('path');
-const errorHandler = require('./helpers/error-handler');
+
 
 app.use(express.static(__dirname + '/libs'));
 
@@ -16,58 +16,43 @@ app.options('*', cors());
 //middleware
 app.use(express.json());
 app.use(morgan('tiny'));
-app.use(errorHandler);
+
 
 //Routes
 const categoriesRoutes = require('./routes/categories');
 const projectsRoutes = require('./routes/projects');
 
-const api = process.env.API_URL;
-const port = process.env.PORT;
-
-
+const api = "api/v1/";
+const port =3000;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 // use res.render to load up an ejs view file
-
 // index page
 app.get(`${api}`, function(req, res) {
     res.render('pages/index');
 });
-
-
 // about page
 app.get(`${api}/about`, function(req, res) {
     res.render('pages/about');
 });
-
-
-
-
-
-
 app.use(`${api}/categories`, categoriesRoutes);
 app.use(`${api}/projects`, projectsRoutes);
-
 //Database
 mongoose
-    .connect(process.env.CONNECTION_STRING, {
+    .connect( 'mongodb+srv://ariel:ahjardines123...@cluster0.taubc.mongodb.net/cuban-api?retryWrites=true&w=majority', {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: process.env.DB_NAME
+        useUnifiedTopology: true
     })
     .then(() => {
-        console.log('we are using ' + process.env.DB_NAME);
-        console.log("🌎 Successfully connected to the database");
+         console.log("🌎 Successfully connected to the database");
     })
     .catch((err) => {
         console.log('😞 Could not connect to the database. Exiting now...', err);
     });
-const PORT = process.env.PORT || 3000;
+const PORT =3000;
 
 //Server
 app.listen(PORT, () => {
-    console.log(`✌server is running http://localhost:${port}`);
+    console.log(`✌server is running http://localhost:3000`);
 });
